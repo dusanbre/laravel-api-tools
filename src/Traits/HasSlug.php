3820a44392828d9d542ef20f18641a3f25f913f1
dusanbre <dusan.antonijevic2404@gmail.com>
2023-05-18 14:19:41 +0200

    Fix unique slug and username

--- src/Traits/HasSlug.php
+++ src/Traits/HasSlug.php
@@ -8,6 +8,19 @@ trait HasSlug
 {
     protected static function bootHasSlug(): void
     {
-        static::creating(static fn(self $model) => $model->slug ?: $model->setAttribute('slug', Str::slug($model->name)));
+        static::creating(static fn(self $model) => $model->slug ?: $model->setAttribute('slug', static::generateUniqueSlug(Str::slug($model->name))));
+    }
+
+    protected static function generateUniqueSlug(string $value): string
+    {
+        $slug = static::query()
+            ->where('slug', 'regexp', preg_quote($value).'[\\d]?$')
+            ->orderByDesc('slug')
+            ->take(1)
+            ->value('slug');
+
+        $number = (int)filter_var($slug, FILTER_SANITIZE_NUMBER_INT);
+
+        return $number === 0 ? $value : $value.++$number;
     }
 }
--- src/Traits/HasUsername.php
+++ src/Traits/HasUsername.php
@@ -8,6 +8,19 @@ trait HasUsername
 {
     protected static function bootHasUsername(): void
     {
-        static::creating(static fn(self $model) => $model->username ?: $model->setAttribute('username', Str::slug($model->name)));
+        static::creating(static fn(self $model) => $model->username ?: $model->setAttribute('username', static::generateUniqueUsername(Str::slug($model->name))));
+    }
+
+    protected static function generateUniqueUsername(string $value): string
+    {
+        $username = static::query()
+            ->where('username', 'regexp', preg_quote($value).'[\\d]?$')
+            ->orderByDesc('username')
+            ->take(1)
+            ->value('username');
+
+        $number = (int)filter_var($username, FILTER_SANITIZE_NUMBER_INT);
+
+        return $number === 0 ? $value : $value.++$number;
     }
 }
